1a926af5c7b8074445e63936f7de127104e225e3
Arc Ranges <arc.ranges@live.ca>
2022-05-28 21:00:00 -0700

    updated donate page

--- _pages/Donate/index.tsx
+++ _pages/Donate/index.tsx
@@ -7,6 +7,8 @@ import HeroBanner from "_pages/Home/HeroBanner";
 
 const PAYPAL_LINK =
   "https://www.paypal.com/donate/?business=8389QZ23QRDPE&no_recurring=0&item_name=Gameserver+community+donataion&currency_code=USD";
+const PAYPAL_LINK_PH =
+  "https://www.paypal.com/donate/?business=BD73SS58WSTCE&no_recurring=0&item_name=Gameserver+community+donation&currency_code=PHP";
 
 export default function index() {
   return (
@@ -16,8 +18,8 @@ export default function index() {
       <div className="pt-20" />
 
       <Container>
-        <div className="mx-auto text-center">
-          <h2 className="font-bold text-white">Donate</h2>
+        <div className="mx-auto text-center mb-10 pb-10 border-b">
+          <h2 className="font-bold text-white">Donate (International)</h2>
           <p className="text-gray-200 mb-3">We appreciate your support</p>
 
           <div className="flex flex-col items-center">
@@ -61,6 +63,58 @@ export default function index() {
             </form>
 
             <Link href={PAYPAL_LINK}>
+              <span className="text-white cursor-pointer hover:opacity-70">
+                Button doesn't show? Cick here.
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        <div className="mx-auto text-center">
+          <h2 className="font-bold text-white">Donate (Philippines)</h2>
+          <p className="text-gray-200 mb-3">We appreciate your support</p>
+
+          <div className="flex flex-col items-center">
+            <div style={{ height: 150, width: 150, marginBottom: 20 }}>
+              <Image
+                src="/img/donate-qr-ph.png"
+                height="150px"
+                width="150px"
+                layout="responsive"
+              />
+            </div>
+            <form
+              className="mb-3"
+              action="https://www.paypal.com/donate"
+              method="post"
+              target="_top"
+            >
+              <div className="flex justify-center">
+                <input type="hidden" name="business" value="BD73SS58WSTCE" />
+                <input type="hidden" name="no_recurring" value="0" />
+                <input
+                  type="hidden"
+                  name="item_name"
+                  value="Gameserver community donation"
+                />
+                <input type="hidden" name="currency_code" value="PHP" />
+                <input
+                  type="image"
+                  src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+                  name="submit"
+                  title="PayPal - The safer, easier way to pay online!"
+                  alt="Donate with PayPal button"
+                />
+                <img
+                  alt=""
+                  src="https://www.paypal.com/en_PH/i/scr/pixel.gif"
+                  width="1"
+                  height="1"
+                />
+              </div>
+            </form>
+
+            <Link href={PAYPAL_LINK_PH}>
               <span className="text-white cursor-pointer hover:opacity-70">
                 Button doesn't show? Cick here.
               </span>
